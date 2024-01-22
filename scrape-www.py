22ff32308e8ee112e18cb92a1f60cd3a3c66e296
researchersec <109282836+researchersec@users.noreply.github.com>
2024-01-22 13:42:03 +0100

    fixed link_element

--- scrape-www.py
+++ scrape-www.py
@@ -16,7 +16,7 @@ with open("www/README.md", "w", encoding="utf-8") as file:
         title_element = result.find("h4").find("a") if result.find("h4") else None
         company_element = result.find("div", class_="jix-toolbar-top__company").find("a")
         location_element = result.find("div", class_="jobad-element-area").find("span") if result.find("div", class_="jobad-element-area") else None
-        link_element = result.find("a", class_="btn btn-sm btn-primary")
+        link_element = result.find("a", class_="btn btn-sm btn-block btn-primary d-md-none mt-2 seejobmobil")
         pub_element = result.find("div", class_="jix-toolbar__pubdate")
 
         if title_element is not None and company_element is not None and location_element is not None:
